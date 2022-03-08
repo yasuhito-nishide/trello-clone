4afb0e38a76e09727530c5c4cbd8489bea491f70
Yasuhito Nishide <yasuhitonishide@gmail.com>
2022-03-09 06:20:18 +0900

    カードタイトル実装

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -2,16 +2,25 @@ import React, { useState } from 'react'
 
 export const TaskCardTitle = () => {
   const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState("Title");
   const handleClick = () => {
     setIsClick(true);
   };
+  const handleChange = (e) => {
+    setInputCardTitle(e.target.value)
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false);
+  }
+  const handleBlur = () => {
+    setIsClick(false)
+  }
+
   return (
-    <div onClick={handleClick}>
-      {isClick ? (
-      <form>
-        <input type="text" />
-      </form>
-      ):<h3>Today</h3>}
+    <div onClick = {handleClick}> {
+      isClick ? (<form onSubmit={handleSubmit}><input type="text" onChange={handleChange} onBlur={handleBlur}/></form>) : <h3>{inputCardTitle}</h3>
+      }
     </div>
   )
 }
