b0642ddfcf66b4dd6247f1e51e6c010e17a100e0
Yasuhito Nishide <yasuhitonishide@gmail.com>
2022-03-26 14:53:35 +0900

    更新

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import { Droppable } from 'react-beautiful-dnd';
+import { DragDropContext } from 'react-beautiful-dnd';
 import { Task } from './Task'
 
 export const Tasks = ({taskList, setTaskList }) => {
   return (
     <div>
-      {taskList.map((task) =>(
-        <div>
-          <Task task={task} taskList={taskList} setTaskList={setTaskList}/>
-        </div>
-      ))}
+      <DragDropContext>
+        <Droppable droppableId='dropapble'>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+            {taskList.map((task) =>(
+              <div>
+                <Task task={task} taskList={taskList} setTaskList={setTaskList}/>
+              </div>
+            ))}
+            </div>
+          )} 
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
